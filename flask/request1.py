841963490f1ecc85ef0f97333470540a0a4cada9
tluulw <tluulw@yandex.ru>
2024-03-16 18:19:51 +0300

    request for database with LIKE

---
+++ flask/request1.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init('db/database.db')
+    db_sess = db_session.create_session()
+    for user in db_sess.query(User).filter((User.position.like('%middle%')) | (User.position.like('%chief%'))).all():
+        print(user.surname, user.name)
+
+
+if __name__ == '__main__':
+    main()
